1692279018c1f20ec5763dc9bc04c58909750bff
stella <sftamesis13@gmail.com>
2022-10-03 14:47:49 -0700

    complete Deliverable 1: add new questions to server

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -16,10 +16,32 @@ function QuestionForm(props) {
       [event.target.name]: event.target.value,
     });
   }
-
+/* Deliverable #2: Creating a new question. 
+    - when X event ocurs (a user submits a form)
+    - Make Y fetch request (POST/items with new item data)
+    - Update Z state (add a new item to state)
+*/
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+
+    const newQuestion = {
+      prompt: formData.prompt,
+      answers: [
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4,
+      ],
+      correctIndex: parseInt(formData.correctIndex)
+    }
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify(newQuestion),
+    })
+
   }
 
   return (
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([])
+  console.log('questions: ', questions)
+
+
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+    .then(response => response.json())
+    .then(data => setQuestions(data))
+  }, [])
+
   return (
     <section>
       <h1>Quiz Questions</h1>
